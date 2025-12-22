7c9b75aeb8538427b25883485be7739ea424238e
Andrew <andrewramirez.toolsfs@gmail.com>
2025-12-22 15:23:34 -0800

    paper dependencies & grade wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,23 @@ plugins {
     id("java")
 }
 
-group = "letsrolldrew-familyfeud"
-version = "1.0-SNAPSHOT"
+group = "io.letsrolldrew.feud"
+version = "0.1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
 
 repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
     mavenCentral()
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -17,4 +26,10 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.processResources {
+    filesMatching("plugin.yml") {
+        expand("version" to project.version)
+    }
 }
