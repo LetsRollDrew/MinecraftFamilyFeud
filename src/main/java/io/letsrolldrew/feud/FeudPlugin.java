95e975f0b25c654e8f6be554585686e6bf9e8b53
Andrew <andrewramirez.toolsfs@gmail.com>
2025-12-22 15:33:02 -0800

    setting up plugin lifecycle for loading/disabling/shutdown

---
+++ src/main/java/io/letsrolldrew/feud/FeudPlugin.java
@@ -0,0 +1,36 @@
+package io.letsrolldrew.feud;
+
+import io.letsrolldrew.feud.bootstrap.PluginBootstrap;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FeudPlugin extends JavaPlugin {
+    private PluginBootstrap bootstrap;
+
+    @Override
+    public void onEnable() {
+        getLogger().info(() -> "Enabling FamilyFeud v" + getDescription().getVersion());
+        bootstrap = new PluginBootstrap(this);
+        try {
+            bootstrap.enable();
+        } catch (Exception ex) {
+            getLogger().severe("Failed to enable FamilyFeud: " + ex.getMessage());
+            ex.printStackTrace();
+            getServer().getPluginManager().disablePlugin(this);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        if (bootstrap != null) {
+            try {
+                bootstrap.disable();
+            } catch (Exception ex) {
+                getLogger().severe("Error during shutdown: " + ex.getMessage());
+                ex.printStackTrace();
+            } finally {
+                bootstrap = null;
+            }
+        }
+        getLogger().info("Disabled FamilyFeud");
+    }
+}
---
+++ src/main/java/io/letsrolldrew/feud/bootstrap/PluginBootstrap.java
@@ -0,0 +1,19 @@
+package io.letsrolldrew.feud.bootstrap;
+
+import org.bukkit.plugin.Plugin;
+
+public final class PluginBootstrap {
+    private final Plugin plugin;
+
+    public PluginBootstrap(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void enable() {
+
+    }
+
+    public void disable() {
+
+    }
+}
